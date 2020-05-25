f41881f171482cd221e3fdf305ac6cfcd351543c
MayurRajBharati <mayur14bharati@gmail.com>
2020-05-25 15:44:06 +0530

    New Error message

--- src/lyrics.js
+++ src/lyrics.js
@@ -4,7 +4,7 @@ const lyrics_get = (song,artist,callback)=>{
     const url = 'http://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track='+ song +'&q_artist='+ artist +'&apikey=19ce464362b98fdd91890c9d93454d48';
     request({url:url, json:true},(error,response)=>{
         if(error){
-            callback('No Internet',undefined);
+            callback('No Internet Service',undefined);
         }
         else if(response.body.message.header.status_code=='404'){
             callback('Invalid Search',undefined);
